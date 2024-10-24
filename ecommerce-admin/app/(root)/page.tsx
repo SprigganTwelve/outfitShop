3111cbb07db83d76372b3be386e3ca266acba566
Paterne <angepaterne.dali@gmail.com>
2024-10-24 16:21:47 +0200

    sign and signin auth has been managed

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
+"use client"
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { useRouter } from 'next/navigation';
+
+export default function SetupPage() {
+    const router = useRouter()
+    const handleClick = () => {
+        router.push('/sign-in/[[...index]].tsx'); 
+      };
     return (
-      <div>
-            <Button>Click Me</Button>
+      <div  className="p-4" >
+            <UserButton />
+            <Button onClick={handleClick}>Click Me</Button>
       </div>
     );
   }
--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Admin Dashborad",
@@ -24,12 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+  <ClerkProvider>
+      <html lang="en">
+          <body>{children}</body>
+      </html>
+  </ClerkProvider>
   );
 }
